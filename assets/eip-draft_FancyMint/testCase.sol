39baf1b115a871e2fe719224943c6d5f1e8e3b40
shypink <shypink@protonmail.com>
2023-01-10 13:39:48 +0000

    + new test case

--- assets/eip-draft_FancyMint/testCase.sol
+++ assets/eip-draft_FancyMint/testCase.sol
@@ -2,28 +2,33 @@
 
 pragma solidity ^0.8.0;
 
-import "./ERC721FancyMint.sol";
+import "./FancyEnumerable_extension/ERC721FancyMintEnumRef.sol";
 import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
-import {Base64} from "@openzeppelin/contracts@4.7.0/utils/Base64.sol"; 
+import {Base64} from "@openzeppelin/contracts@4.7.0/utils/Base64.sol";
 
-contract Fancy2 is ERC721FancyMint, Ownable {
+contract Fancy2 is ERC721FancyMintEnum, Ownable {
     using Strings for uint256;
 
     //
-    address private preOwner_ = 0x42D184ccD43e84368ea95902847D137f5C49704C;
-    uint256 private maxSupply_ = 6000;
+    address private preOwner_ = 0x66aB6D9362d4F35596279692F0251Db635165871;
+    uint256 private maxSupply_ = 5000;
     //
-    
-    string private head = '<svg width="240" height="80" viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg"><rect width="200" height="40" style="fill:rgb(255,0,255);stroke-width:3;stroke:rgb(0,0,0)" /><text x="20" y="70" >';
+
+    string private head =
+        '<svg width="240" height="80" viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg"><rect width="200" height="40" style="fill:rgb(255,0,255);stroke-width:3;stroke:rgb(0,0,0)" /><text x="20" y="70" >';
     string private tail = "</text></svg>";
 
     constructor()
         ERC721FancyMint("Fancy second try", "fst", maxSupply_, preOwner_)
     {}
 
+    function singleMint() public {
+        _mint(msg.sender, totalSupply());
+    }
+
     function generateSVG(uint256 id) internal view returns (string memory) {
         // string memory _svgString = head;
-        
+
         return string(abi.encodePacked(head, id.toString(), tail));
     }
 
@@ -48,8 +53,6 @@ contract Fancy2 is ERC721FancyMint, Ownable {
                 )
             );
     }
-
-
 
     function tokenURI(uint256 id)
         public
